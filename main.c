bf52f9d62feab7a9716879dbea9ff65c2de51892
YEONGJUN LEE <adone@ingris.com>
2023-12-15 13:46:08 +0900

    main store

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    int i;
+
+    if (argc != 3) {
+        printf("Usage : a.out [gb2 file] [path]");
+        return 1;
+    }
+
+    char command[128];
+    char *kw_command = "kwgrib2 ";
+    char *txt_opt = "-text ";
+
+    char *fname = argv[1];
+    char *filename = strrchr(fname, '.');
+    char *path = argv[2];
+
+    char out_path[64];
+    strcat(out_path, path);
+    strcat(out_path, "/");
+    strcat(out_path, filename);
+    strcat(out_paht, ".txt");
+
+    strcat(command, kw_command);
+    strcat(command, fname);
+    strcat(command, " ");
+    strcat(command, txt_opt);
+    strcat(command, filename);
+    strcat(command, out_path);
+
+    printf("command = %s\n", command);
+
+
+    return 0;
+}
